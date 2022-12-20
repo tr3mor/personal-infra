cd3d7f8f9ec1992829ef5b5b1dac111974423e94
tr3mor <storozhenkopf@gmail.com>
2022-12-20 14:54:32 +0100

    feat(grafana-oncall): create ws for grafana oncall

--- terraform/tf-cloud/workspaces/grafana-cloud-oncall-ws.tf
+++ terraform/tf-cloud/workspaces/grafana-cloud-oncall-ws.tf
@@ -5,7 +5,7 @@ module "grafana_oncall_tfe_workspace" {
   description = "Workspace to manage Grafana Oncall in Grafana Cloud"
   org_name    = tfe_organization.tr3mor.name
 
-  working_directory   = "terraform/grafana-cloud/grafana-                       oncall"
+  working_directory   = "terraform/grafana-cloud/grafana-oncall"
   vcs_repo_identifier = "tr3mor/personal-infra"
   oauth_token_id      = tfe_oauth_client.github.oauth_token_id
 
