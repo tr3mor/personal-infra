368f97667140eb5f27367abfc8cd2b41b20703a2
tr3mor <storozhenkopf@gmail.com>
2023-03-04 16:10:48 +0100

    add github-repo ws

--- terraform/tf-cloud/workspaces/grafana-cloud-stack-ws.tf
+++ terraform/tf-cloud/workspaces/grafana-cloud-stack-ws.tf
@@ -9,6 +9,8 @@ module "grafana_stack_tfe_workspace" {
   vcs_repo_identifier = "tr3mor/personal-infra"
   oauth_token_id      = tfe_oauth_client.github.oauth_token_id
 
+  trigger_patterns = ["terraform-modules/github/repos/*", "terraform/github/repos/*"]
+
   notification_email_user_ids = [data.tfe_organization_membership.admin.user_id]
   create_variable_set         = true
 }
