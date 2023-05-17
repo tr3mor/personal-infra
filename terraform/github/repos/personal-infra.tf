12c9a1e6dcd95f6da2ed1b4540b80ae6cdd146ab
tr3mor <storozhenkopf@gmail.com>
2023-05-17 13:28:10 +0200

    add description for personal infra

--- terraform/github/repos/personal-infra.tf
+++ terraform/github/repos/personal-infra.tf
@@ -1,5 +1,6 @@
 module "personal-infra" {
   source = "../../../terraform-modules/github/github-repo"
 
-  name = "personal-infra"
+  name        = "personal-infra"
+  description = "This repo implements GitOps approach to manage my personal infra"
 }
