ba8deff047407d8c0c1fd4b060fd085628dd839c
Pavel Storozhenko <storozhenkopf@gmail.com>
2026-09-20 21:39:10 +0200

    Require build status check on pizza-calculator main branch (#1873)

--- terraform/github/repos/pizza-calculator.tf
+++ terraform/github/repos/pizza-calculator.tf
@@ -5,4 +5,8 @@ module "pizza-calculator" {
   auto_init        = true
   pages_enabled    = true
   pages_build_type = "workflow"
+
+  required_status_checks = [
+    "build", # ci.yml, runs npm ci && npm run build on every PR
+  ]
 }
